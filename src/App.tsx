4a91a24a125e054790c1c77ea008736e38d53086
Lakshan Costa <lakshanrcosta@gmail.com>
2024-05-27 22:14:19 +0300

    Completed building base ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <main>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="total-text">0</h1>
+        <div className="input-container">
+          <input type="text" placeholder="Income or expense" />
+          <input type="number" />
+          <select>
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+          </select>
+          <button>+</button>
+        </div>
+        <div className="card">
+          <div className="card-info">
+            <h4>Salary</h4>
+            <p>July 27th 2024</p>
+          </div>
+          <p className="amount-text success">+ $5000</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
